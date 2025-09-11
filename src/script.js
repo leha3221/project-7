dc84d676d1def319257054670f821301b2c2c429
leha3221 <maslov3221@gmail.com>
2025-09-11 15:27:39 +0300

    поправил вывод ошибки

--- src/script.js
+++ src/script.js
@@ -1,38 +1,41 @@
-const form= document.getElementById('ContactForm');
-const emailInput=document.getElementById('email');
+const form=document.getElementById('ContactForm');  //получаем элемент формы
+const emailInput=document.getElementById('Email');//получаем поле ввода email
+const button=document.getElementById('Submit');
 
-emailInput.addEventListener('input',validateEmail);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateEmail()
+}); //на событие ввода email будет вызываться функция validateEmail()
 
-function validateEmail() {
-    const emalRegex=/[A-Za-z0-9_\.\-]{2,10}@[a-z0-9_\.\-]{1,10}\.[a-z]{2,3}/;
-    if(emailRegex.test(emailInput.value)){
-        removeError(emailInput);
-        return true;
+function validateEmail(){
+  //функция проверки корректности email
+  const emailRegex=/[A-Za-z0-9_\-\.]{2,10}@[a-z0-9\.\-_]{1,10}\.[a-z]{2,3}/;
+  if(emailRegex.test(emailInput.value)){
+    removeError(emailInput);
+    return True;
 
-    }   
-    else{
-        showError(emailInput,"Email не соответсвует формату");
-        return false;
-    }
+  }
+  else{
+    showError(emailInput, "Email не соответствует формату. Пример: ivanov@mail.ru");
+    return false;
+  }
 }
+function showError(input, message){   //функция показа ошибки
+  const formControl = input.parentElement;  //получаем родительскую форму
+  const errorElement = formControl.querySelector('error') || document.createElement('div');
 
-function showError(input, message){
-    const formControl=input.parentElement;
-    const errorElement=formControl.querySelector('error') || document.createElement('div');
+  errorElement.className='error';
+  errorElement.textContent = message;
 
-    errorElement.className='error';
-    errorElement.textContent=message;
-
-    formControl.appendChild(errorElement);
-    input.style.borderColor='red';
+  formControl.appendChild(errorElement);
+  input.style.borderColor = 'red';
 }
+function removeError(input){    //функция скрытия ошибки
+  const formControl = input.parentElement;
+  const errorElement = formControl.querySelector('error');
 
-function removeError(input){
-    const formControl=input.parentElement;
-    const errorElement=formControl.querySelector('error');
-
-    if(errorElement){
-        formControl.removeChild(errorElement);
-    }
-    input.style.borderColor='green';
+  if(errorElement){
+    formControl.removeChild(errorElement);
+  }
+  input.style.borderColor = 'green';
 }
